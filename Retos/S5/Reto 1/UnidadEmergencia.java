32f823535a5a1abae51d744d0acaa2f9d6f3ddb4
andrea-26181 <and594.a@gmail.com>
2025-05-19 13:04:58 -0600

    Update UnidadEmergencia.java

--- Retos/S5/Reto 1/UnidadEmergencia.java
+++ Retos/S5/Reto 1/UnidadEmergencia.java
@@ -1,5 +1,18 @@
+//1. Crea una clase abstracta UnidadEmergencia
+
+//La primera línea en un archivo Java debe indicar a que paquete pertenece la clase
 package Solucion_Reto_01;
 
+//Se crea una clase abstracta 
+/*
+-una clase abstracta es una clase que no puede ser instanciada directamente, es 
+decir, no se pueden crear objetos de ella
+-Sirve como plantilla para otras clases que la heredan, obligándolas a implementar 
+sus métodos abstractos (aquellos sin implementación)
+-Las clases abstractas son útiles para definir una funcionalidad común a un grupo
+de subclases, dejando que estas definan la implementación de ciertos 
+comportamientos específicos.
+*/
 public abstract class UnidadEmergencia {
     String nombre;
 
